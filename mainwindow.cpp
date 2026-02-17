f3306219626ad8e7d5d7ea81177875b58d7c7e6c
Huskarl80 <antioh.iv@gmail.com>
2026-02-17 14:34:13 +0300

    add button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,52 +3,16 @@
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QFont>
-
-void MainWindow::addlabel()
-{
-    int spacing = 100;
-    int fontSize = 24;
-
-    auto *label2 = new QLabel ("Slovo");
-    auto *central = new QWidget(this);
-    auto *layout  = new QVBoxLayout(central);
-
-    auto *label = new QLabel("Hello, Qt!", central);
-
-    // Создаем шрифт нужного размера
-    QFont font;
-    font.setPointSize(fontSize);
-
-    // Применяем шрифт к обоим label'ам
-    label2->setFont(font);
-    label->setFont(font);
-
-    // Синий цвет текста
-    label2->setStyleSheet("color: blue;");
-    label->setStyleSheet("color: blue;");
-
-    layout->addWidget(label2);
-    layout->addSpacing(spacing);
-    layout->addWidget(label);
-    layout->setSpacing(0);
-
-    label2->setAlignment(Qt::AlignCenter);
-    label->setAlignment(Qt::AlignCenter);
-    layout->setAlignment(Qt::AlignCenter);
-
-    central->setStyleSheet("background-color: rgb(127, 255, 0);");
-
-    central->setLayout(layout);
-    setCentralWidget(central);
-}
+#include <QPushButton> //  инклудим кнопку
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Privet");
-    this->addlabel();
+    setWindowTitle("Privet");
+    addlabel();
 }
 
 MainWindow::~MainWindow()
@@ -56,5 +20,64 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::addlabel() //  наша функция. разрослась конечно. потом может подумать про более нарядное разделение.
+{
+    int spacing = 80;
+    int fontSize = 24;
 
+    auto *central = new QWidget(this);
+    auto *layout  = new QVBoxLayout(central);
 
+    auto *yellowRect = new QPushButton("", central);
+    yellowRect->setFixedSize(100, 50);
+    yellowRect->setStyleSheet(  //  тут окантовка
+        "QPushButton {"
+        "    background-color: yellow;"
+        "    border: 2px solid black;"
+        "    border-radius: 0;"
+        "}"
+        "QPushButton:pressed {"
+        "    background-color: orange;"
+        "}"
+        );
+
+    connect(yellowRect, &QPushButton::clicked, this, &MainWindow::onYellowRectClicked);
+
+    labelX = new QLabel("X", central);
+    auto *label = new QLabel("Hello, Qt!", central);
+
+    QFont font;
+    font.setPointSize(fontSize);
+    labelX->setFont(font);
+    label->setFont(font);
+
+    labelX->setStyleSheet("color: blue;");
+    label->setStyleSheet("color: blue;");
+
+    layout->addWidget(yellowRect, 0, Qt::AlignCenter);
+    layout->addWidget(labelX);
+    layout->addSpacing(spacing);//  здесь пространство между для верстки - не потерять.
+    layout->addWidget(label);
+    layout->setSpacing(0);
+
+    labelX->setAlignment(Qt::AlignCenter);
+    label->setAlignment(Qt::AlignCenter);
+    layout->setAlignment(Qt::AlignCenter);
+
+    central->setStyleSheet("background-color: rgb(127, 255, 0);");//  если что могу прям их фотошопа добавить кодировку цвета.
+    central->setLayout(layout);
+    setCentralWidget(central);
+}
+
+void MainWindow::onYellowRectClicked() //  мой флип/флоп
+{
+    isXVisible = !isXVisible;
+
+    if (isXVisible) {
+        labelX->show();
+        qDebug() << "X показан";
+    } else {
+        labelX->hide();
+        qDebug() << "X скрыт";
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>  // нужно это инклудить - QLabel*
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -14,11 +14,18 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);//  Здесь объявляем nullptr чтобы отказаться от родителя.
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
     void addlabel();
+    QLabel *labelX = nullptr;     //  Активируем nullptr
+    bool isXVisible = true;       //  переменная булка
+
+private slots:
+    void onYellowRectClicked();
 };
+
 #endif // MAINWINDOW_H
+
